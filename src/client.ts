377624f7193620ebf9f12ad37eb97315f832acdb
DiscordFiles <93401523+DiscordFiles@users.noreply.github.com>
2022-07-13 13:14:03 +0200

    changement pour interface

--- src/client.ts
+++ src/client.ts
@@ -1,5 +1,4 @@
-import discord from "discord.js"
-
-export default class Client extends discord.Client {
-  public commands = new discord.Collection<string, Command>()
+export interface Command {
+  build: SlashCommandBuilder
+  run: (interaction: Interaction) => unknown
 }
